81aef7fff4e31c157d54c7780cadd3f4fc847a38
jabracadabra <bitbucket_adimin_service@medifastinc.com>
2026-09-22 22:27:30 -0500

    Updated version numbers

--- LeafVillageLegends/Core/Constants.lua
+++ LeafVillageLegends/Core/Constants.lua
@@ -1,6 +1,6 @@
 _G = _G or (getfenv and getfenv(0)) or {}
 
-LATEST_VERSION = "19.1.0"
+LATEST_VERSION = "19.1.1"
 LEAFVE_STYLE = _G.LeafVE_Styles or {}
 LEAFVE_UI_MODERN = _G.LeafVE_UIModernization or {}
 
